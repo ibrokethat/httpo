c1cde8d158d6ca49291b79449ffc482d38e0bf55
Simon Jefford <si@ibrokethat.com>
2014-07-25 15:37:52 +0100

    er

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,16 @@ use lib::experiments::{test_proc, test_ref, test_struct};
 //	define the empty dependency module structure used to find modules on the filesystem
 mod lib { pub mod experiments;}
 
-fn main() {
+// fn main() {
 
-	// test_proc();
-	// test_ref();
-	// test_struct();
-	test_vec_func();
-}
+// 	// test_proc();
+// 	// test_ref();
+// 	// test_struct();
+// 	test_vec_func();
+// }
 
 
-struct Httpo {
+pub struct Httpo {
 	middleware: Vec<fn(&mut Context)>,
 	server: int
 }
@@ -25,9 +25,10 @@ impl Httpo {
 		Httpo {middleware: Vec::new(), server: 10i}
 	}
 
-	fn utilise (mut self, mw: fn(&mut Context)) {
-		self.middleware.push(mw);
+	fn utilise (mut self, mw: &fn(&mut Context)) {
+		self.middleware.push(*mw);
 	}
+
 }
 
 struct Context {
@@ -58,18 +59,42 @@ fn test_vec_func () {
 	// v.push(mid_1);
 	// v.push(mid_2);
 
-	let mut app = box Httpo::new();
+	// let app = box Httpo::new();
+	// let ref a = *app;
+	// a.utilise(&mid_1);
+	// a.utilise(&mid_2);
 
-	app.utilise(mid_1);
-	app.utilise(mid_2);
+	// let mut ctx = Context {status: 200, body: String::new()};
 
-	let mut ctx = Context {status: 200, body: String::new()};
+	// for f in app.middleware.iter() {
+	// 	let func = *f;
+	// 	func(&mut ctx);
+	// }
 
-	for f in app.middleware.iter() {
-		let func = *f;
-		func(&mut ctx);
-	}
+	// println!("middleware: {}", ctx.body);
 
-	println!("middleware: {}", ctx.body);
+}
 
+
+struct Monster {
+    health: int,
+    attack: int
+}
+
+impl Monster {
+    fn attack(&self, i: int) {
+        println!("The monster attacks for {:d} damage. {:d}", self.attack, i);
+    }
+
+    fn count() {
+        println!("There are a bunch of monsters out tonight.");
+    }
+}
+
+fn main() {
+    let m = Monster { health: 10, attack: 20 };
+
+    m.attack(5i);
+    m.attack(10i);
+    Monster::count();
 }
