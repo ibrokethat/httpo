0e36ecf0b4b54cfb182e010c0c06c2fd1c98e872
Simon Jefford <si@ibrokethat.com>
2014-07-29 13:54:18 +0100

    it compiled - woohA

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,24 @@ use lib::experiments::{test_proc, test_ref, test_struct};
 //	define the empty dependency module structure used to find modules on the filesystem
 mod lib { pub mod experiments;}
 
-// fn main() {
+fn main() {
 
-// 	// test_proc();
-// 	// test_ref();
-// 	// test_struct();
-// 	test_vec_func();
-// }
-
+	test_proc();
+	test_ref();
+	test_struct();
+	test_vec_func();
+}
 
 pub struct Httpo {
 	middleware: Vec<fn(&mut Context)>,
 	server: int
+}
+
+impl Clone for Httpo {
+  fn clone (&self) -> Httpo {
+    let mut middleware = self.middleware.iter().map(|x| *x);
+    Httpo {middleware: middleware.collect(), server: self.server }
+  }
 }
 
 impl Httpo {
@@ -25,7 +31,7 @@ impl Httpo {
 		Httpo {middleware: Vec::new(), server: 10i}
 	}
 
-	fn utilise (mut self, mw: &fn(&mut Context)) {
+	fn utilise (&mut self, mw: &fn(&mut Context)) {
 		self.middleware.push(*mw);
 	}
 
@@ -51,7 +57,6 @@ fn mid_2 (ctx: &mut Context) {
 }
 
 
-
 fn test_vec_func () {
 
 	// let mut v = Vec::new();
@@ -59,42 +64,24 @@ fn test_vec_func () {
 	// v.push(mid_1);
 	// v.push(mid_2);
 
-	// let app = box Httpo::new();
-	// let ref a = *app;
-	// a.utilise(&mid_1);
-	// a.utilise(&mid_2);
+	let app = Httpo::new();
+	let mut a = app.clone();
+	a.utilise(&mid_1);
+	let mut b = a.clone();
+	b.utilise(&mid_2);
 
-	// let mut ctx = Context {status: 200, body: String::new()};
-
-	// for f in app.middleware.iter() {
-	// 	let func = *f;
-	// 	func(&mut ctx);
-	// }
-
-	// println!("middleware: {}", ctx.body);
-
-}
+	// app.clone().utilise(&mid_1);
+	// app.clone().utilise(&mid_2);
 
 
-struct Monster {
-    health: int,
-    attack: int
-}
+	let mut ctx = Context {status: 200, body: String::new()};
 
-impl Monster {
-    fn attack(&self, i: int) {
-        println!("The monster attacks for {:d} damage. {:d}", self.attack, i);
-    }
+	for f in b.middleware.iter() {
+		println!("fuuuccckkkkkkkkkk");
+		let func = *f;
+		func(&mut ctx);
+	}
+	println!("middleware length: {}", b.middleware.len());
+	println!("ctx body: {}", ctx.body);
 
-    fn count() {
-        println!("There are a bunch of monsters out tonight.");
-    }
-}
-
-fn main() {
-    let m = Monster { health: 10, attack: 20 };
-
-    m.attack(5i);
-    m.attack(10i);
-    Monster::count();
 }
